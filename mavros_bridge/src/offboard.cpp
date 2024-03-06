dc5c76ef047326b1ad5c9491962adfb755c03ba1
aravind <sai.aravind@newspace.co.in>
2024-03-06 18:03:28 +0530

    code doc

--- mavros_bridge/src/offboard.cpp
+++ mavros_bridge/src/offboard.cpp
@@ -1,9 +1,3 @@
-/**
- * @file offb_node.cpp
- * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
- * Stack and tested in Gazebo Classic SITL
- */
-
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/CommandBool.h>
@@ -16,7 +10,6 @@ ros::ServiceClient set_mode_client;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
-void shutdown_Handler(int sig);
 
 int main(int argc, char **argv)
 {
@@ -42,27 +35,29 @@ int main(int argc, char **argv)
         rate.sleep();
     }
 
+    //mavros msg for arming
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    //mavros msg for setting the mode
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+
+    //Goal point  w.r.t local frame
     geometry_msgs::PoseStamped pose;
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = 2;
 
-    //send a few setpoints before starting
-    /*for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
-    }*/
-
-    mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
-
-    mavros_msgs::CommandBool arm_cmd;
-    arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
 
     while(ros::ok()){
+        /*
+        Set the drone to offboard mode for controlling in the autonomous mode 
+        -> Current drone mode is published in /mavros/state 
+        */
         if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
             if( set_mode_client.call(offb_set_mode) &&
@@ -71,6 +66,7 @@ int main(int argc, char **argv)
             }
             last_request = ros::Time::now();
         } else {
+            // once the drone is in offboard mode we can arm the drone
             if( !current_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0))){
                 if( arming_client.call(arm_cmd) &&
@@ -81,7 +77,7 @@ int main(int argc, char **argv)
             }
         }
 
-        local_pos_pub.publish(pose);
+        local_pos_pub.publish(pose); // publish the goal point 
 
         ros::spinOnce();
         rate.sleep();
